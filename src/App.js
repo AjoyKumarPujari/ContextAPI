d12393f1aa12e049af5815b0e657b57f3d0b386e
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-04-27 08:18:58 +0530

    api use

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import Pagination from "./components/Pagination";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
 import "./App.css"
+import { Route, useLocation, useSearchParams } from "react-router-dom";
 
 export default function App() {
   const {fetchBlogPosts} = useContext(AppContext);
 
+  const [searchParams, setSearchParams]=useSearchParams();
+  const location = useLocation();
+
   useEffect(() => {
-    fetchBlogPosts();
-  },[]);
+    //fetchBlogPosts();
+    const page = searchParams.get("page") ?? 1;
+
+    if(location.pathname.includes("tags")){
+      //tag wala page show karna hai
+      const tag = location.pathname.split("/").at(-1).replace("-"," ");
+      fetchBlogPosts(Number(page), tag);
+    }
+    else if(location.pathname.includes("categories")){
+      const category =location.pathname.split("/").at(-1).replace("-"," ");
+      fetchBlogPosts(Number(page), null, category);
+    }
+    else{
+      fetchBlogPosts(Number(page));
+    }
+  },[location.pathname,location.search ]);
 
   return (
-    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
-      <Header />
-      <Blogs />
-      <Pagination />
-    </div>
+    // <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+    //   <Header />
+    //   <Blogs />
+    //   <Pagination />
+    // </div>
+    <Routes>
+      <Route path="/" element= {<Home/>} />
+      <Route path="/blog/:blogId" element= {<BlogPage/>} />
+      <Route path="/tags/:tag" element= {<TagPage/>} />
+      <Route path="/categories/:category" element= {<CategoryPage />} />
+    </Routes>
   );
 
 }
