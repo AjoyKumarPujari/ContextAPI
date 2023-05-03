ab7dd33dd4ff4d181ef6b5ba23aaa99582ecb19c
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-05-03 22:49:03 +0530

    Add Routes in app.js

---
+++ src/Pages/TagPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const TagPage = () => {
+  return (
+    <div>
+        <Header/>
+        <div>
+        <button
+        >
+        back
+        </button>
+        </div>
+    </div>
+    
+  )
+}
+
+export default TagPage
